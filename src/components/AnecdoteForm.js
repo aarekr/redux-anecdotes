142565e7d89c7917999967dfdcaddb84fd3cb5cc
aarekr <aare212@hotmail.com>
2022-11-08 08:30:25 +0200

    Tehtävä 6.14 step2

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,15 +1,17 @@
 import { useDispatch } from "react-redux"
 import { createAnecdote } from "../reducers/anecdoteReducer"
+import anecdoteService from "../services/anecdotes"
 
 const AnecdoteForm = (props) => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         console.log('lisätään anecdote content:', content)
         event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(createAnecdote(newAnecdote))
     }
 
     return (
